994fbb1314e27c4c62ba0dbaaf3c00b1c8de2e46
Sreekanth Munigati <smunigati@HW13919.local>
2018-09-06 17:18:36 -0400

    Added Nifi flows with kafka druid

--- src/hive_ddl/hive_druid.sql
+++ src/hive_ddl/hive_druid.sql
@@ -53,3 +53,53 @@ CREATE external TABLE hotel_transactions_druid
 STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler' TBLPROPERTIES ("druid.segment.granularity"="MONTH","druid.query.granularity"="DAY") AS SELECT cast(transaction_dt_timestamp as timestamp) as `__time`,transaction_dt,check_in,check_out,metro,property, cast(price_paid as string) as price_paid_s ,from_unixtime(unix_timestamp(transaction_dt,'mm-dd-yyyy'),'E') as transaction_in_week, number_rooms,number_of_audults,number_of_childern FROM hotel_transactions_csv where transaction_dt_timestamp is not null;
 
 
+
+
+CREATE EXTERNAL TABLE kafka_hotel_enquiries_druid(
+`__time` timestamp,
+check_in string,
+check_out string,
+metro string,
+property string,
+quoted_price string,
+number_of_audults string,
+number_of_childern string,
+number_rooms string)
+STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
+TBLPROPERTIES (
+"kafka.bootstrap.servers" = "ip-172-31-26-98.ec2.internal:6667",
+"kafka.topic" = "hotel_enquiries",
+"druid.kafka.ingestion.useEarliestOffset" = "true",
+"druid.kafka.ingestion.maxRowsInMemory" = "5",
+"druid.kafka.ingestion.startDelay" = "PT1S",
+"druid.kafka.ingestion.period" = "PT1S",
+"druid.kafka.ingestion.consumer.retries" = "2",
+"druid.kafka.ingestion" = 'START'
+)
+
+
+
+CREATE EXTERNAL TABLE kafka_hotel_transactions_druid(
+`__time` timestamp,
+check_in string,
+check_out string,
+metro string,
+property string,
+paid_price string,
+customer_id string,
+number_of_audults string,
+number_of_childern string,
+number_rooms string)
+STORED BY 'org.apache.hadoop.hive.druid.DruidStorageHandler'
+TBLPROPERTIES (
+"kafka.bootstrap.servers" = "ip-172-31-26-98.ec2.internal:6667",
+"kafka.topic" = "hotel_transactions",
+"druid.kafka.ingestion.useEarliestOffset" = "true",
+"druid.kafka.ingestion.maxRowsInMemory" = "5",
+"druid.kafka.ingestion.startDelay" = "PT1S",
+"druid.kafka.ingestion.period" = "PT1S",
+"druid.kafka.ingestion.consumer.retries" = "2",
+"druid.kafka.ingestion" = 'START'
+)
+
+
